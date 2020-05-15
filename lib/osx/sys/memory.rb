792268f43aaf99b6930da50cd10e9ec3c73d9b52
Daniel Berger <djberg96@gmail.com>
2020-05-15 14:28:35 -0400

    Calculate page size, return a hash with more info.

--- lib/osx/sys/memory.rb
+++ lib/osx/sys/memory.rb
@@ -9,6 +9,7 @@ module Sys
     HOST_VM_INFO64_COUNT = 38
 
     attach_function :sysctlbyname, [:string, :pointer, :pointer, :pointer, :size_t], :int
+    attach_function :host_page_size, [:pointer, :pointer], :int
     attach_function :host_statistics64, [:pointer, :int, :pointer, :pointer], :int
     attach_function :mach_host_self, [], :pointer
 
@@ -48,13 +49,25 @@ module Sys
       size = FFI::MemoryPointer.new(:size_t)
       size.write_int(optr.size)
 
+      hash = {}
+
       if sysctlbyname('hw.memsize', optr, size, nil, 0) < 0
         raise Error, "sysctlbyname function failed"
       end
 
       memsize = optr.read_uint64
 
+      hash[:total_memory] = memsize
+
       host_self = mach_host_self()
+
+      psize = FFI::MemoryPointer.new(:uint)
+
+      rv = host_page_size(host_self, psize)
+      raise SystemCallError.new('host_page_size', rv) if rv != 0
+
+      page_size = psize.read_uint
+
       vmstat = VmStat.new
       count = FFI::MemoryPointer.new(:size_t)
       count.write_int(vmstat.size)
@@ -62,17 +75,18 @@ module Sys
       rv = host_statistics64(host_self, HOST_VM_INFO64, vmstat, count)
       raise SystemCallError.new('host_statistics64', rv) if rv != 0
 
-=begin
-      p vmstat[:active_count]
-      p vmstat[:inactive_count]
-      p vmstat[:speculative_count]
-      p vmstat[:wire_count]
-=end
+      hash[:active] = vmstat[:active_count] * page_size
+      hash[:inactive] = vmstat[:inactive_count] * page_size
+      hash[:speculative] = vmstat[:speculative_count] * page_size
+      hash[:wire] = vmstat[:wire_count] * page_size
+      hash[:compressed] = vmstat[:compressor_page_count] * page_size
 
-      memsize
+      hash
     ensure
-      size.free
-      optr.free
+      size.free if size && !size.null?
+      optr.free if optr && !optr.null?
+      count.free if count && !count.null?
+      psize.free if psize && !psize.null?
     end
 
     module_function :memory
