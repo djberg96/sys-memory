fee27e6eb5585ce51a8b96e7cb238362cd49aaf2
Daniel Berger <djberg96@gmail.com>
2020-05-26 19:08:49 -0400

    Add total, free, used, load methods for Linux.

--- lib/sys/linux/memory.rb
+++ lib/sys/linux/memory.rb
@@ -7,7 +7,6 @@ module Sys
       regex = /(.*)?:\s+?(\d+)/
 
       IO.foreach(MEMORY_FILE) do |line|
-        #p line
         key, value = regex.match(line.chomp).captures
         hash[key] = value.to_i
       end
@@ -15,6 +14,25 @@ module Sys
       hash
     end
 
-    module_function :memory
+    # Total available physical memory.
+    #
+    def total
+      memory['MemTotal'] * 1024
+    end
+
+    def free
+      memory['MemFree'] * 1024
+    end
+
+    def used
+      hash = memory
+      (hash['MemTotal'] - hash['MemFree'] - hash['Buffers'] - hash['Cached'] - hash['Slab']) * 1024
+    end
+
+    def load
+      (used / total.to_f).round(2) * 100
+    end
+
+    module_function :memory, :total, :free, :used, :load
   end
 end
