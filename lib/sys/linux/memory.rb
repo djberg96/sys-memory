0204e5a9470f13901a4242e44d96dc336df02c6d
Daniel Berger <djberg96@gmail.com>
2020-05-27 17:06:59 -0400

    Update Linux methods to allow swap to be optionally included in calculations.

--- lib/sys/linux/memory.rb
+++ lib/sys/linux/memory.rb
@@ -14,23 +14,39 @@ module Sys
       hash
     end
 
-    # Total available physical memory.
+    # Total memory in bytes. By default this is only physical memory, but
+    # if the +extended+ option is set to true, then swap memory is included
+    # as part of the total.
     #
-    def total
-      memory['MemTotal'] * 1024
-    end
-
-    def free
-      memory['MemFree'] * 1024
-    end
-
-    def used
+    def total(extended: false)
       hash = memory
-      (hash['MemTotal'] - hash['MemFree'] - hash['Buffers'] - hash['Cached'] - hash['Slab']) * 1024
+      extended ? (hash['MemTotal'] + hash['SwapTotal']) * 1024 : hash['MemTotal'] * 1024
     end
 
-    def load
-      (used / total.to_f).round(2) * 100
+    # The memory currently available, in bytes. By default this is only
+    # physical memory, but if the +extended+ option is set to true, then free
+    # swap memory is also included.
+    #
+    def free(extended: false)
+      hash = memory
+      extended ? (hash['MemFree'] + hash['SwapFree']) * 1024 : hash['MemFree'] * 1024
+    end
+
+    # The memory, in bytes, currently in use. By default this is only
+    # physical memory, but if the +extended+ option is set to true then
+    # swap is included in the calculation.
+    #
+    def used(extended: false)
+      hash = memory
+      (total(extended) - free(extended) - hash['Buffers'] - hash['Cached'] - hash['Slab']) * 1024
+    end
+
+    # A number between 0 and 100 that specifies the approximate percentage of
+    # memory that is in use. If the +extended+ option is set to true then
+    # swap memory is included in the calculation.
+    #
+    def load(extended: false)
+      (used(extended) / total(extended).to_f).round(2) * 100
     end
 
     module_function :memory, :total, :free, :used, :load
